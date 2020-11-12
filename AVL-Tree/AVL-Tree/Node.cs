9af24c42b0dace351e09f1b6f6c6d2d4dc1bce7d
Tamara Mayer <110002@fhwn.ac.at>
2020-11-12 20:10:41 +0100

    restored project from other branch that is now deleted because it was wrong

---
+++ AVL-Tree/AVL-Tree/Node.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AVL_Tree
+{
+    public class Node
+    {
+        //as leafnode is 0, only 0,1,or -1 is an aceptable value, otherwise turn tree
+        public int BalanceFactor { get; set; }
+        public int Height { get; set; }
+        public int Value { get; set; }
+
+        public Node Parent { get; set; }
+        public Node Left { get; set; }
+
+        public Node Right { get; set; }
+
+
+        public Node(int value, Node parent)
+        {
+            this.Value = value;
+            this.Height = 1;
+            this.Parent = parent;
+        }
+
+    }
+}
---
+++ AVL-Tree/AVL-Tree/Tree_ViewModel.cs
@@ -0,0 +1,133 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+using System.Windows.Media;
+
+namespace AVL_Tree
+{
+    class Tree_ViewModel : INotifyPropertyChanged
+    {
+        private int inputField;
+
+        private string messageForUser;
+        private SolidColorBrush messageColor;
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        public string MessageForUser
+        {
+            get
+            {
+                return this.messageForUser;
+            }
+            set
+            {
+                this.messageForUser = value;
+                this.Notify();
+            }
+        }
+        public int InputField
+        {
+            get
+            {
+                return this.inputField;
+            }
+            set
+            {
+                this.inputField = value;
+                this.Notify();
+            }
+        }
+
+        public SolidColorBrush MessageColor
+        {
+            get
+            {
+                return this.messageColor;
+            }
+            set
+            {
+                this.messageColor = value;
+                this.Notify();
+            }
+        }
+
+        public Node root { get; set; }
+
+        public ObservableCollection<Node> Tree { get; set; }
+
+        public Tree_ViewModel()
+        {
+            Tree = new ObservableCollection<Node>();
+        }
+
+        public ICommand InsertCommand
+        {
+            get
+            {
+                return new Command
+                (
+                    obj =>
+                    {
+
+                        if (root == null)
+                        {
+                            root = new Node(this.InputField);
+                        }
+                        else
+                        {
+                            RecurviseInsert(root);
+                        }
+
+                    });
+            }
+        }
+
+        private void RecurviseInsert(Node currentNode)
+        {
+            if (currentNode.Value == inputField)
+            {
+                this.InputField = 0;
+                this.MessageForUser = "This number is already in the tree. No duplicate allowed!";
+                this.MessageColor = new SolidColorBrush(Color.FromRgb(255, 0, 0));
+            }
+            else
+            {
+                if (currentNode.Value > this.inputField)
+                {
+                    if(currentNode.Left == null)
+                    {
+                        currentNode.Left = new Node(inputField, currentNode);
+                    }
+                    else
+                    {
+                        RecurviseInsert(currentNode.Left);
+                    }
+                }
+                else
+                {
+                    if (currentNode.Right == null)
+                    {
+                        currentNode.Right = new Node(inputField, currentNode);
+                    }
+                    else
+                    {
+                        RecurviseInsert(currentNode.Right);
+                    }
+                }
+            }
+
+        }
+
+        private void Notify([CallerMemberName] string property = null)
+        {
+            this.PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(property));
+        }
+    }
+}
