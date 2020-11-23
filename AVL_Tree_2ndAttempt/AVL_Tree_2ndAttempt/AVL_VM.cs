ae8880f2b3cb8de75e647384ea7da78d6eb61d5c
Tamara Mayer <110002@fhwn.ac.at>
2020-11-23 21:00:34 +0100

    added projects

---
+++ AVL_Tree_2ndAttempt/AVL_Tree_2ndAttempt/AVL_VM.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AVL_Tree_2ndAttempt
+{
+    class AVL_VM
+    {
+        Node root;
+    }
+}
