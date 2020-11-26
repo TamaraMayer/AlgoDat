f5267573b6bfa9184a53901c65f65796293ba691
Tamara Mayer <110002@fhwn.ac.at>
2020-11-26 13:55:11 +0100

    created Test project

---
+++ AVL_Tree/AVL_Tree_Test/UnitTest1.cs
@@ -0,0 +1,14 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace AVL_Tree_Test
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void Should_InsertSomeValues_usingInsertCommand()
+        {
+
+        }
+    }
+}
