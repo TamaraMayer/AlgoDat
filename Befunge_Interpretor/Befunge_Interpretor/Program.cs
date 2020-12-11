2b4f46672eb6ad89641f24529f30c124a57d7549
Tamara Mayer <tamara-mayer@outlook.com>
2020-12-11 13:55:24 +0100

    created Solution in new Branch

---
+++ Befunge_Interpretor/Befunge_Interpretor/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Befunge_Interpretor
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
