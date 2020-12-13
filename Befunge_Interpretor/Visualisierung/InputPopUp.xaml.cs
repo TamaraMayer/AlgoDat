2c64464dff26042fe3fc588d7ddb566dd1ba07e0
Tamara Mayer <tamara-mayer@outlook.com>
2020-12-13 22:02:34 +0100

    added a WPF project for Visualization, added the IInputVisitor

---
+++ Befunge_Interpretor/Visualisierung/InputPopUp.xaml.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace Visualisierung
+{
+    /// <summary>
+    /// Interaktionslogik für InputPopUp.xaml
+    /// </summary>
+    public partial class InputPopUp : Window
+    {
+        public InputPopUp()
+        {
+            InitializeComponent();
+        }
+
+        private void enter_btn_click(object sender, RoutedEventArgs e)
+        {
+            DialogResult = true;
+        }
+    }
+}
---
+++ Befunge_Interpretor/Visualisierung/InputVisitor.cs
@@ -0,0 +1,24 @@
+﻿using Befunge_Interpretor;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Visualisierung
+{
+    public class InputVisitor : IInputVisitor
+    {
+        public string GetUserInput(string messageToUser)
+        {
+            InputPopUp instance = new InputPopUp();
+            instance.MessageToUser.Text= messageToUser;
+
+           if(instance.ShowDialog() == true)
+            {
+                string input = instance.MessageToUser.Text;
+                instance.Close();
+                return input;
+            }
+            return "";
+        }
+    }
+}
